3d65740fd0f95fa83b1d085e004ad7c43bdea4d3
Francisco Bernabe Santamaria Mancia <sm19035@ues.edu.sv>
2023-10-05 14:58:21 -0600

    cambios a vista dashboard

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -13,9 +14,11 @@ class PostController extends Controller
     }
 
     public function index(User $user)
-    {       
+    {
+        $post=Post::where('user_id',$user->id)->get();      
         return view('dashboard',[
-            'user'=>$user
+            'user'=>$user,
+            'posts'=>$post
         ]);
     }
 
@@ -32,5 +35,31 @@ class PostController extends Controller
             'imagen'=>'required'
 
         ]);
+
+        //primera forma
+        // Post::create([
+        //     'titulo'=>$request->titulo,
+        //     'descripcion'=>$request->descripcion,
+        //     'imagen'=>$request->imagen,
+        //     'user_id'=>auth()->user()->id
+        // ]);
+
+        //segunda forma
+        // $post=new Post;
+        // $post->titulo=$request->titulo;
+        // $post->descripcion=$request->descripcion;
+        // $post->imagen=$request->imagen;
+        // $post->user_id=auth()->user()->id;
+        // $post->save();
+
+        //3ra forma
+        $request->user()->posts()->create([
+            'titulo'=>$request->titulo,
+            'descripcion'=>$request->descripcion,
+            'imagen'=>$request->imagen,
+            'user_id'=>auth()->user()->id
+        ]);
+
+        return redirect()->route('posts.index',auth()->user()->username);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,10 +8,11 @@
     <div class="flex justify-center">
         <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
             <div class="w-8/12 lg:w-6/12 px-5">
-                <img src="{{asset('img/usuario.svg')}}" alt="Usuario">
+                <img src="{{ asset('img/usuario.svg') }}" alt="Usuario">
             </div>
-            <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:justify-center py-10 md:py-10">
-                <p class="text-gray-700 text-2xl">{{$user->username}}</p>
+            <div
+                class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:justify-center py-10 md:py-10">
+                <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
                 <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
                     0
                     <span class="font-normal">Seguidores</span>
@@ -30,4 +31,22 @@
             </div>
         </div>
     </div>
+
+    <section class="container mx-auto mt-10" style="padding: 3%">
+        <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
+        @if ($posts->count())
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                @foreach ($posts as $post)
+                    <div>
+                        <a>
+                            <img src="{{ asset('uploads') . '/' . $post->imagen }}"
+                                alt="Imagen del post {{ $post->titulo }}">
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        @else
+            <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
+        @endif
+    </section>
 @endsection
