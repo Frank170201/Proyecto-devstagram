311286e051bcac791bfc699df79c6f8f6ada35cd
Francisco Santamaria <franciscobernabesantamariamanc@gmail.com>
2023-10-16 23:02:10 -0600

    subiendo cambios de controlador y eso

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -67,7 +67,7 @@ class PostController extends Controller
     {
         return view('posts.show',[
             'post'=>$post,
-            // 'user'=>$user
+            'user'=>$user
         ]);
     }
 }
