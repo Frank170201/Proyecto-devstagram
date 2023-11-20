e1c86904903f59d7d1a8f8a85e76264c6f2baeee
Francisco Bernabe Santamaria Mancia <sm19035@ues.edu.sv>
2023-11-20 12:00:42 -0600

    agregando cambios

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -15,8 +15,9 @@ class LikeController extends Controller
        ]);
        return back();
     }
-    public function destroy()
+    public function destroy(Request $request,Post $post)
     {
-        
+        $request->user()->likes()->where('post_id',$post->id)->delete();
+        return back();
     }
 }
